7b6f5dcba600bf9e5b453ae0627f19f019ba7a0e
mukadder <mukadder@bu.edu>
2018-02-13 16:11:10 -0500

    added Object Factory classe

--- src/main/java/com/arpit/react/app/ReactAppApplication.java
+++ src/main/java/com/arpit/react/app/ReactAppApplication.java
@@ -4,23 +4,35 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.kuali.rice.kim.v2_0.CreateEntity;
+import org.kuali.rice.kim.v2_0.CreateEntityResponse;
+import org.kuali.rice.kim.v2_0.EntityType;
 import org.kuali.rice.kim.v2_0.GetEntityByEmployeeId;
 import org.kuali.rice.kim.v2_0.GetEntityByEmployeeIdResponse;
 import org.kuali.rice.kim.v2_0.IdentityService;
 import org.kuali.rice.kim.v2_0.IdentityService_Service;
+import org.kuali.rice.kim.v2_0.ObjectFactory;
 import org.kuali.rice.kim.v2_0.RiceIllegalArgumentException;
+import org.kuali.rice.kim.v2_0.RiceIllegalStateException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.kuali.rice.kim.v2_0.ObjectFactory;
 import edu.bu.kuali.rice.kew.client.KSBCampusServiceClient;
 
 @SpringBootApplication
 @RestController
 public class ReactAppApplication {
-
+	//private ObjectFactory objectFactory;
 	public static void main(String[] args) {
 		SpringApplication.run(ReactAppApplication.class, args);
 	}
@@ -28,18 +40,78 @@ public class ReactAppApplication {
 	@GetMapping("/employee/get")
 	public List<Employee> get() throws RiceIllegalArgumentException {
 		URL wsdlURL = IdentityService_Service.WSDL_LOCATION;
-	      KSBCampusServiceClient client = new KSBCampusServiceClient();
+		KSBCampusServiceClient client = new KSBCampusServiceClient();
 
-	      IdentityService svc = client.getCampusService( wsdlURL );
-			//Campuses campuses = svc.findAllCampuses();
-	      GetEntityByEmployeeId id= new GetEntityByEmployeeId();
-	      id.setEmployeeId("U17116978");
-	     GetEntityByEmployeeIdResponse test=   svc.getEntityByEmployeeId(id);
+		IdentityService svc = client.getCampusService(wsdlURL);
+		// Campuses campuses = svc.findAllCampuses();
+		ObjectFactory factory = new ObjectFactory();
+
+		GetEntityByEmployeeId id = factory.createGetEntityByEmployeeId();;
+		id.setEmployeeId("U17116978");
+		GetEntityByEmployeeIdResponse test = svc.getEntityByEmployeeId(id);
 
 		List<Employee> employeeList = new ArrayList<>();
-		employeeList.add(new Employee( test.getEntity().getPrincipals().getPrincipal().get(0).getEntityId().toString(),  test.getEntity().getPrincipals().getPrincipal().get(0).getPrincipalName(), test.getEntity().getNames().getName().get(0).getFirstName().toString()));
+		employeeList.add(new Employee(test.getEntity().getPrincipals().getPrincipal().get(0).getEntityId().toString(),
+				test.getEntity().getPrincipals().getPrincipal().get(0).getPrincipalName(),
+				test.getEntity().getNames().getName().get(0).getFirstName().toString()));
 		return employeeList;
 	}
+//http://localhost:8080/employee/get/U17116978
+	@GetMapping("/employee/get/{id}")
+	public ResponseEntity getCustomer(@PathVariable("id") String id) throws RiceIllegalArgumentException {
+		URL wsdlURL = IdentityService_Service.WSDL_LOCATION;
+		KSBCampusServiceClient client = new KSBCampusServiceClient();
+
+		IdentityService svc = client.getCampusService(wsdlURL);
+		// Campuses campuses = svc.findAllCampuses();
+		GetEntityByEmployeeId idd = new GetEntityByEmployeeId();
+		idd.setEmployeeId(id);
+		GetEntityByEmployeeIdResponse test = svc.getEntityByEmployeeId(idd);
+
+		List<Employee> employeeList = new ArrayList<>();
+		employeeList.add(new Employee(test.getEntity().getPrincipals().getPrincipal().get(0).getEntityId().toString(),
+				test.getEntity().getPrincipals().getPrincipal().get(0).getPrincipalName(),
+				test.getEntity().getNames().getName().get(0).getFirstName().toString()));
+
+		return new ResponseEntity(employeeList.get(0), HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/employee/createEmployee")
+	public ResponseEntity createEmployee(@RequestBody Employee employee) throws RiceIllegalArgumentException, RiceIllegalStateException {
+
+		URL wsdlURL = IdentityService_Service.WSDL_LOCATION;
+		KSBCampusServiceClient client = new KSBCampusServiceClient();
+
+		IdentityService svc = client.getCampusService(wsdlURL);
+		CreateEntity createNewEntity = new CreateEntity();
+	      EntityType type = new EntityType();
+
+	      type.setActive(true);
+	     // type.setAffiliations(value);
+	      createNewEntity.setEntity(type);
+
+	      CreateEntityResponse  response= svc.createEntity(createNewEntity);
+
+		return new ResponseEntity(employee, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/employee/{id}")
+	public ResponseEntity deleteEmployee(@PathVariable String id) {
+
+		return new ResponseEntity(id, HttpStatus.OK);
+
+	}
+
+	@PutMapping("/employee/{id}")
+	public ResponseEntity updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+
+		if (null == employee) {
+			return new ResponseEntity("No Employee found for ID " + id, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity(employee, HttpStatus.OK);
+	}
+
 }
 
 @Controller
@@ -52,7 +124,6 @@ class IndexPageController {
 }
 
 final class Employee {
-
 
 	private String id;
 	private String name;
